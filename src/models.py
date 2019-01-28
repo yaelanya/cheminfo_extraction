58b598331c60f20dac2b7216814f0f61f96d9045
yaelanya <asahi.hentona@gmail.com>
2019-01-28 17:16:25 +0900

    add attention layer

--- src/models.py
+++ src/models.py
@@ -35,3 +35,43 @@ class BiLM(torch.nn.Module):
         backward_mask = torch.cat((inputs[-1:], inputs[:-1])) > 2
         
         return forward_mask, backward_mask
+
+
+class Attention(torch.nn.Module):
+    def __init__(self, embedding_dim):
+        super(Attention, self).__init__()
+
+        self.W = torch.nn.Linear(embedding_dim, embedding_dim, bias=False)
+
+
+    def forward(self, word_embs, sentence_embs):
+        """
+        Args:
+            word_embs: (batch_size, seq_len, embedding_dim)
+            sentence_embs: (batch_size, num_sentence, embedding_dim)
+        """
+
+        batch_size, seq_len, embedding_dim = word_embs.size()
+        num_sentence = sentence_embs.size(1)
+
+        # Calculate h_1,t*W_a
+        word_embs = word_embs.view(-1, embedding_dim)
+        word_embs = self.W(word_embs)
+        word_embs = word_embs.view(batch_size, seq_len, embedding_dim)
+
+        # Caluclate score=(h_1,t*W_a)*s_j
+        # att_scores shape: (batch_size, seq_len, num_sentence)
+        att_scores = torch.bmm(word_embs, sentence_embs.transpose(1, 2))
+
+        # Caluclate attention weights
+        att_scores = att_scores.view(-1, num_sentence)
+        att_weights = torch.nn.functional.softmax(att_scores, dim=-1)
+        att_weights = att_weights.view(batch_size, seq_len, num_sentence)
+
+        # shape: (batch_size, seq_len, embedding_dim)
+        g = torch.bmm(att_weights, sentence_embs)
+
+        # shape: (batch_size, seq_len, 2*embedding_dim)
+        combination = torch.cat((word_embs, g), dim=-1)
+
+        return combination, g
