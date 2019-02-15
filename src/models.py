3afc5eabafdf554fd54893f70ab80eba20cea55b
yaelanya <asahi.hentona@gmail.com>
2019-02-15 11:34:56 +0900

    add pack-pad processing

--- src/models.py
+++ src/models.py
@@ -429,20 +429,31 @@ class Att_BiLSTM_CRF(BiLSTM_CRF):
         batch_size = inputs.size(0)
 
         embeds = self.word_embeds(word_inputs) # (batch_size, seq_len, embedding_dim)
-        embeds = embeds.transpose(0, 1) # (seq_len, batch_size, embedding_dim)
-        lstm1_out, _ = self.lstm_1(self.dropout(embeds)) # (seq_len, batch_size, 2*lstm1_units)
-        lstm1_out = lstm1_out.transpose(0, 1) # (batch_size, seq_len, 2*lstm1_units)
+        embeds = self.dropout(embeds)
+
+        # sort index
+        lens = (word_inputs > self.tag_to_ix[self.PAD_TAG]).sum(-1)
+        sorted_lens, sorted_indices = lens.sort(descending=True)
+        _, origin_indices = sorted_indices.sort()
+
+        embeds = embeds[sorted_indices]
+        packed = pack_padded_sequence(embeds, sorted_lens, batch_first=True)
+        lstm1_out, _ = self.lstm_1(packed)
+        lstm1_out, _ = pad_packed_sequence(lstm1_out, batch_first=True) # (batch_size, seq_len, 2*lstm1_units)
+
         if sent_embs is not None:
-            attention_out, _ = self.att(lstm1_out, sent_embs) # (batch_size, seq_len, 2*2*lstm1_units)
+            attention_out, _ = self.att(lstm1_out, sent_embs[sorted_indices]) # (batch_size, seq_len, 2*2*lstm1_units)
         else:
             attention_out, _ = self.att(lstm1_out, lstm1_out)
-
-        attention_out = attention_out.transpose(1, 0) # (seq_len, batch_size, 2*2*lstm1_units)
-        lstm2_out, _ = self.lstm_2(attention_out) # (seq_len, batch_size, 2*lstm2_units)
-        lstm2_out = lstm2_out.transpose(1, 0) # (batch_size, seq_len, 2*lstm2_units)
         
+        packed = pack_padded_sequence(attention_out, sorted_lens, batch_first=True)
+        lstm2_out, _ = self.lstm_2(packed)
+        lstm2_out, _ = pad_packed_sequence(lstm2_out, batch_first=True) # (batch_size, seq_len, 2*lstm2_units)
+
         lstm2_out = lstm2_out.contiguous()
         lstm2_out = lstm2_out.view(-1, 2*self.lstm2_units) # (batch_size*seq_len, 2*lstm2_units)
         lstm_feats = self.hidden2tag(lstm2_out) # (batch_size*seq_len, tagset_size)
+        lstm_feats = lstm_feats.view(batch_size, -1, self.tagset_size) # (batch_size, seq_len, tagset_size)
+        lstm_feats = lstm_feats[origin_indices]
 
-        return lstm_feats.view(batch_size, -1, self.tagset_size) # (batch_size, seq_len, tagset_size)
+        return lstm_feats
